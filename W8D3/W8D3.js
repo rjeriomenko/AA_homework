f2a7b9130ddbac825a0dc5e42e50e0b3729494d1
rokasj <rokas@jerio.me>
2023-04-26 11:49:49 -0400

    Finish HW1 Phase 2

--- W8D3/W8D3.js
+++ W8D3/W8D3.js
@@ -1,20 +1,41 @@
 function titleize(nameArr, cb) {
-    const titles = nameArr.map(function(ele, i) {
+    const titles = nameArr.map(function (ele, i) {
         return `Mx. ${ele} "Mr. Grips" Burnette` ;
     });
 
-    titles.forEach(function(ele) {
+    titles.forEach(function (ele) {
         console.log(ele)
     });
 }
 
 titleize(["Stefan", "Flatlander"])
 
-function Elephant() {
-    this.hi = function() {
-        console.log("hi")
-    }
+function Elephant(name, height, tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
 }
 
-let kek = Elephant();
-kek.hi();
+Elephant.prototype.trumpet = function () {
+    console.log(`${this.name} the giraffe goes "kekekekekekekek"`);
+}
+
+Elephant.prototype.grow = function () {
+    this.height += 12;
+}
+
+Elephant.prototype.addTrick = function (trick) {
+    this.tricks.push(trick);
+}
+
+Elephant.prototype.play = function () {
+    console.log(this.tricks[Math.floor(Math.random() * this.tricks.length)])
+}
+
+let kek = new Elephant("Chungus", 9000, ["doing a flip"]);
+kek.trumpet();
+kek.grow();
+console.log(kek.height);
+kek.addTrick("standing on hooves")
+console.log(kek.tricks)
+kek.play()
